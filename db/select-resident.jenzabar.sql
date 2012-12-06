ec51bd9caa01280b3652d4303ec4af3d60e65e90
adamanthil <andrew@bender.io>
2012-12-06 02:50:31 -0600

    updating jenzabar query to include candidate residents

--- db/select-resident.jenzabar.sql
+++ db/select-resident.jenzabar.sql
@@ -1,28 +1,26 @@
-SELECT stsd.id_num,
+SELECT  nm.id_num,
         nm.first_name,
         nm.last_name,
         bm.gender,
         bm.birth_dte,
         addr.addr_line_1 as email,
         nm.mobile_phone,
-        stsd.career_hrs_earned as TotalCredits,
-        stsd.hrs_enrolled as EnrolledCredits,
+        COALESCE(stsd.career_hrs_earned, 0) as TotalCredits,
+        COALESCE(stsd.hrs_enrolled, 0) as EnrolledCredits,
         CASE WHEN c.stage IS NOT NULL AND c.stage <> 'DEP' THEN 'false' ELSE 'true' END as eligible,
         CASE WHEN c.id_num IS NOT NULL THEN 'New' ELSE 'Returning' END as residenttype,
         CASE WHEN ext.udef_1a_1 = 'A' THEN 'true' ELSE 'false' END as ApprovedException
-FROM stud_term_sum_div stsd
-INNER JOIN name_master nm
-	ON nm.id_num = stsd.id_num
+FROM name_master nm
 INNER JOIN biograph_master bm
-	ON bm.id_num = stsd.id_num
+    ON bm.id_num = nm.id_num
 INNER JOIN addr_all_email_addrs addr
-	ON addr.id_num = stsd.id_num
+    ON addr.id_num = nm.id_num
+LEFT JOIN stud_term_sum_div stsd
+    ON stsd.id_num = nm.id_num
 LEFT JOIN candidacy c
-	ON c.yr_cde = stsd.yr_cde
-	AND c.trm_cde = stsd.trm_cde
-	AND c.id_num = stsd.id_num
+    ON c.id_num = nm.id_num
 LEFT JOIN stud_sess_asgn_ext ext
     ON ext.id_num = stsd.id_num
     AND ext.sess_cde = $%$SESS_CDE$%$
-WHERE stsd.yr_cde = $%$YR_CDE$%$
-AND stsd.trm_cde = $%$TRM_CDE$%$
+WHERE (c.yr_cde = $%$YR_CDE$%$ AND c.trm_cde = $%$TRM_CDE$%$)
+OR (stsd.yr_cde = $%$YR_CDE$%$ AND stsd.trm_cde = $%$TRM_CDE$%$)
