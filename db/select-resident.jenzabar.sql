54013a34d4128985618f56c0a784df3d2ec3e1c8
Andrew Bender <andrew@bender.io>
2013-06-07 00:39:37 -0500

    update new/returning logic for jenzabar query

--- db/select-resident.jenzabar.sql
+++ db/select-resident.jenzabar.sql
@@ -9,7 +9,7 @@ SELECT  nm.id_num,
         COALESCE(stsd.hrs_enrolled, 0) as EnrolledCredits,
         COALESCE(stsd.career_gpa, 0) as GPA,
         CASE WHEN c.stage IS NOT NULL AND c.stage <> 'DEP' THEN 'false' ELSE 'true' END as eligible,
-        CASE WHEN stsd.id_num IS NOT NULL AND stsd.career_hrs_earned > 0 THEN 'Returning' ELSE 'New' END as residenttype,
+        CASE WHEN c.id_num IS NOT NULL THEN 'New' ELSE 'Returning' END as residenttype,
         CASE WHEN ext.udef_1a_1 = 'A' THEN 'true' ELSE 'false' END as ApprovedException
 FROM name_master nm
 INNER JOIN biograph_master bm
@@ -18,12 +18,16 @@ INNER JOIN addr_all_email_addrs addr
     ON addr.id_num = nm.id_num
 LEFT JOIN stud_term_sum_div stsd
     ON stsd.id_num = nm.id_num
+    AND stsd.yr_cde = $%$YR_CDE$%$
+    AND stsd.trm_cde = $%$TRM_CDE$%$
 LEFT JOIN candidacy c
     ON c.id_num = nm.id_num
+    AND c.yr_cde = $%$YR_CDE$%$
+    AND c.trm_cde = $%$TRM_CDE$%$
+    AND c.stage IN ('DEP', 'WDDEP', 'ENR')
 LEFT JOIN stud_sess_asgn_ext ext
     ON ext.id_num = stsd.id_num
     AND ext.sess_cde = $%$SESS_CDE$%$
-WHERE (
-    (c.yr_cde = $%$YR_CDE$%$ AND c.trm_cde = $%$TRM_CDE$%$ AND c.stage IN ('DEP', 'WDDEP', 'ENR')) OR
-    (stsd.yr_cde = $%$YR_CDE$%$ AND stsd.trm_cde = $%$TRM_CDE$%$)
-) AND bm.birth_dte IS NOT NULL AND bm.gender IS NOT NULL
+WHERE (c.id_num IS NOT NULL OR stsd.id_num IS NOT NULL)
+AND bm.birth_dte IS NOT NULL
+AND bm.gender IS NOT NULL
